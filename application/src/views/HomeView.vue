511ef00458d65a5f2c7559bdc6415e81aa8430d1
Mike Roguez <mike.roguez@gmail.com>
2026-01-06 13:55:14 -0600

    fix: Fix home and organizers pages

--- application/src/views/HomeView.vue
+++ application/src/views/HomeView.vue
@@ -5,6 +5,7 @@ import TheSpeakers from '@/components/TheSpeakers.vue'
 import TheSpetialThanks from '@/components/SpetialThanks.vue'
 import TheDescription from '@/components/Description.vue'
 import TheOrganization from '@/components/TheOrganization.vue'
+import TheSupporters from '@/components/TheSupporters.vue'
 import TheCoordinationOfProgram from '@/components/TheCoordinationOfProgram.vue'
 import TheSupport from '@/components/Support.vue'
 </script>
@@ -17,6 +18,7 @@ import TheSupport from '@/components/Support.vue'
   <TheDescription />
   <TheOrganization />
   <TheCoordinationOfProgram />
+  <TheSupporters />
   <!-- TheVirtualVenues /> -->
   <!-- <TheSupport /> -->
 </template>
