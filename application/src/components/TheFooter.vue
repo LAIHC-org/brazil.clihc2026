e69bdecd9c382419ccf153caaa145502ac953b78
Mike Roguez <mike.roguez@gmail.com>
2026-01-05 18:42:05 -0600

    docs: :bookmark: v2026.1.0.7

--- application/src/components/TheFooter.vue
+++ application/src/components/TheFooter.vue
@@ -153,7 +153,7 @@
                             {{ $t("nav.copyright") }}
                         </p>
                         <p class="my-0  text-sm">
-                            <a class="uline" href="CHANGELOG.md">v2026.1.0.6</a>
+                            <a class="uline" href="CHANGELOG.md">v2026.1.0.7</a>
                         </p>
                     </div>
                 </div>
